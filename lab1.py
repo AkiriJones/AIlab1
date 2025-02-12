a71c2da13b3cb6c5ce82e4da72c3efe6f7518ac9
Akiri Jones <aj1974@rit.edu>
2025-02-12 17:41:01 -0500

    Remodeled the search, and how the total distance is calculated slightly

--- lab1.py
+++ lab1.py
@@ -36,20 +36,31 @@ def heuristic(curr, goal):
     return max((abs(goal[0] - int(curr[0])), abs(goal[1] - int(curr[1])))) * currDiff
 
 
-def astar(start, checkList: list[list[str]], total_meters: int):
+def getTotalDistance(path: list[list[int]]) -> int:
+    path = path[::-1]
+    total_meters = 0
+    for i in range(len(path) - 1):
+            x1, x2 = path[i][0], path[i + 1][0]
+            y1, y2 = path[i][1], path[i + 1][1]
+            abs_x = abs(x1 - x2)
+            abs_y = abs(y1 - y2)
+            total_meters += (abs_x * xinmeters) + (abs_y * yinmeters)
+    return total_meters
+
+def astar(start: Node, checkPoint: list[int]) -> list[Node] | None:
     """Finds the shortest path using the A* algorithm."""
     open_list = []
     closed_set = set()
-    visited_points = [start]
-    # start_node = Node(None)
+    # visited_points = [Node]
+    start_node = Node(None,start)
     # start_node.setPosition(start.getPosition())
     # visited_points.append(start)
-    end_coords = [int(checkList[0][0]), int(checkList[0][1])]
-    end_node = Node(None, end_coords)
-    end_node.setPosition(end_coords)
+    # end_coords = [int(checkList[0][0]), int(checkList[0][1])]
+    # end_node = Node(None, end_coords)
+    # end_node.setPosition(end_coords)
 
-    heapq.heappush(open_list, start)
-    checkList.remove(checkList[0])
+    heapq.heappush(open_list, start_node)
+    # checkList.remove(checkList[0])
     counter = 0
     while open_list:
         current_node = heapq.heappop(open_list)  # Get node with lowest f-score
@@ -58,45 +69,61 @@ def astar(start, checkList: list[list[str]], total_meters: int):
         # print()
 
         closed_set.add(tuple(current_node.position))
-        # print()
-        if isgoalpoint(current_node.position, checkList):
-            # print("point " + str(checkList[0]) + "reached")
-            checkList.remove(checkList[0])
-            visited_points.append(current_node)
-            closed_set.clear()
-            # open_list.clear()
-            if checkList:
-                end_coords = [int(checkList[0][0]), int(checkList[0][1])]
-                end_node.setPosition(end_coords)
-
-        if isgoal(visited_points):
+        if isgoalpoint(current_node.position, checkPoint):
             path = []
             while current_node:
                 path.append(current_node.position)
-                if current_node != start:
-                    x1, x2 = current_node.getPosition()[0], current_node.parent.getPosition()[0]
-                    y1, y2 = current_node.parent.getPosition()[1], current_node.parent.getPosition()[1]
-                    abs_x = abs(x1 - x2)
-                    abs_y = abs(y1 - y2)
-                    total_meters += (abs_x * xinmeters) + (abs_y * yinmeters)
                 current_node = current_node.parent
+            return path[::-1]
 
-            return path[::-1], total_meters  # Return reversed path and total meters traversed
+        # if isgoal(visited_points):
+        #     path = []
+        #     while current_node:
+        #         path.append(current_node.position)
+        #         # if current_node != start:
+        #         #     x1, x2 = current_node.getPosition()[0], current_node.parent.getPosition()[0]
+        #         #     y1, y2 = current_node.parent.getPosition()[1], current_node.parent.getPosition()[1]
+        #         #     abs_x = abs(x1 - x2)
+        #         #     abs_y = abs(y1 - y2)
+        #         #     total_meters += (abs_x * xinmeters) + (abs_y * yinmeters)
+        #         current_node = current_node.parent
+        #
+        #     return path[::-1]  # Return reversed path and total meters traversed
 
         neighbors = checkNeighbors(current_node.position[0], current_node.position[1])
-        neighbors = getBestNeighbor(closed_set, current_node, neighbors, checkList)
+        neighbors = getBestNeighbor(closed_set, current_node, neighbors, checkPoint)
         for neighbor in neighbors:  # Possible moves
             nbrCoords = neighbor
             neighbor = Node(current_node, nbrCoords)
-            neighbor.setPosition(nbrCoords)
             neighbor.g = current_node.g + 1
-            neighbor.h = heuristic(neighbor.position, end_node.position)
+            neighbor.h = heuristic(neighbor.position, checkPoint)
             neighbor.f = neighbor.g + neighbor.h
             if any(n.position == neighbor.position and n.g <= neighbor.g for n in open_list):
                 continue
             if tuple(neighbor.position) not in closed_set:
                 heapq.heappush(open_list, neighbor)
     return None
+
+# def getTotalDistance(list[Node]) -> int:
+#
+#     return 0
+
+def multi_stage_astar(start, waypoints, goal):
+    total_path = []
+    current_position = start.getPosition()
+
+    for waypoint in waypoints:  # Move through each intermediate goal
+        # point_node = Node(None, waypoint)
+        path_segment = astar(current_position,waypoint)
+        if path_segment is None:
+            return None  # No valid path exists
+
+        total_path.extend(path_segment[:-1])  # Avoid duplicate points
+        current_position = waypoint  # Move to the next stage
+
+    total_path.append(goal.position)  # Add final goal
+    return total_path
+
 
 
 def isgoal(n) -> bool:
@@ -110,12 +137,11 @@ def isgoal(n) -> bool:
     return True
 
 
-def isgoalpoint(coords, checkList: list[list[str]]) -> bool:
-    if not checkList:
+def isgoalpoint(coords, checkpoint: list[int]) -> bool:
+    if not checkpoint:
         return False
     else:
-        point = checkList[0]
-        return abs(coords[0] - int(point[0])) <= 1 and abs(coords[1] - int(point[1])) <= 1
+        return abs(coords[0] - checkpoint[0]) <= 1 and abs(coords[1] - checkpoint[1]) <= 1
 
 
 def getElevations(filename) -> dict:
@@ -167,7 +193,11 @@ def getPath(filename):
     path = []
     file = open(filename, 'r')
     for line in file:
-        path.append(line.split())
+        line = line.split()
+        xy = []
+        for nums in line:
+            xy.append(int(nums))
+        path.append(xy)
     return path
 
 
@@ -186,16 +216,15 @@ def checkNeighbors(oldX: str, oldY: str) -> list[list[int]]:
 
 
 def getBestNeighbor(currlist: set[tuple[list[int]]], curr: Node, neighbors: list[list[int]],
-                    checklist: list[list[str]]) -> list[list[int]]:
-    point = checklist[0]
-    point_x = int(point[0])
-    point_y = int(point[1])
+                    checkpoint: list[int]) -> list[list[int]]:
+    point_x = checkpoint[0]
+    point_y = checkpoint[1]
     currX_abs = abs(curr.position[0] - point_x)
     currY_abs = abs(curr.position[1] - point_y)
 
     nbrs = []
     for nbr in neighbors:
-        if not difficultyMap[colorCoords[nbr[0], nbr[1]]] <= 1:
+        if difficultyMap[colorCoords[nbr[0], nbr[1]]] <= 1:
             if not currlist.__contains__(tuple(nbr)):
                 nbrX_abs = abs(nbr[0] - point_x)
                 nbrY_abs = abs(nbr[1] - point_y)
@@ -214,13 +243,13 @@ if __name__ == '__main__':
         elevation = args[1]
         path_filename = args[2]
         output_filename = args[3]
-        image = Image.open(image)
-        image.convert('RGB')
-        img_width, img_height = image.size
-        image.save('terrain.png')
+        map = Image.open(image)
+        map.save(image)
+        map.convert('RGB')
+        img_width, img_height = map.size
         elevationCoords = getElevations(elevation)
         # image.show()
-        pixels = image.load()
+        pixels = map.load()
         # 395 rows of 500 cols representing each pixel's color on a graph.
         colorCoords = {}
         coords = set()
@@ -229,35 +258,25 @@ if __name__ == '__main__':
             for y in range(int(img_height)):
                 coord = [x, y]
                 coords.add(tuple(coord))
-                coordColor = pixels[x, y]
+                coordColor = pixels[x, y][:-1]
                 colorCoords[tuple(coord)] = coordColor
                 colorSet.add(coordColor)
         path = getPath(path_filename)
         # 1 being the easiest, 0 being the hardest
-        # print(colorSet)
         difficultyMap = getDifficulties(colorSet)
-        # print(difficultyMap)
         goalPath = path.copy()
-        # goalPath.remove(goalPath[0])
-        startcoords = [int(path[0][0]), int(path[0][1])]
+        goalPath.remove(goalPath[0])
+        startcoords = [path[0][0], path[0][1]]
         startNode = Node(None, startcoords)
-        startNode.setPosition(startcoords)
-        traversedPath = []
-        try:
-            traversedPath, total_meters = astar(startNode, goalPath, total_meters)
-        except ValueError:
-            exit(1)
-        except TypeError:
-            print("TypeError")
+        endcoords = [path[len(path)-1][0], path[len(path)-1][1]]
+        endNode = Node(None, endcoords)
+        traversedPath = multi_stage_astar(startNode,goalPath,endNode)
+        print(getTotalDistance(traversedPath))
         if traversedPath:
             drawing_path = []
             for point in traversedPath:
                 drawing_path.append(tuple(point))
-            path_drawing = ImageDraw.Draw(image)
+            path_drawing = ImageDraw.Draw(map)
             path_drawing.line(drawing_path, fill='#a146dd', width=1)
-        else:
-            print("No solution.")
-            sys.exit(0)
-        print(total_meters)
-        image.save(output_filename, format='PNG')
-        image.show()
+            map.save(output_filename, format='PNG')
+            map.show()
